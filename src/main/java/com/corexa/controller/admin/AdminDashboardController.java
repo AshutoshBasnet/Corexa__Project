9ef63881cf00b8119d906ae7ec0e609ce24cd444
AshutoshBasnet <romenrengs102@gmail.com>
2026-05-16 13:43:14 +0545

    admin and services

---
+++ src/main/java/com/corexa/controller/admin/AdminDashboardController.java
@@ -0,0 +1,68 @@
+package com.corexa.controller.admin;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.corexa.service.AdminDashboardService;
+import com.corexa.service.OrderService;
+import com.corexa.util.RedirectionUtil;
+
+/**
+ * Servlet implementation for handling dashboard-related HTTP requests.
+ * 
+ * This servlet manages interactions with the AdminDashboardService to fetch users
+ * information. 
+ *
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/adminDashboard" })
+public class AdminDashboardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// Instance of DashboardService for handling business logic
+	private AdminDashboardService dashboardService;
+	private OrderService orderService;
+	
+	/**
+	 * Default constructor initializes the dashboardService and orderService instance.
+	 */
+	public AdminDashboardController() {
+		
+		try {
+            this.orderService = new OrderService();
+            this.dashboardService = new AdminDashboardService();
+        } catch (SQLException e) {
+            // Log the error and handle it appropriately
+            e.printStackTrace();
+            throw new RuntimeException("Failed to initialize OrderService", e);
+        }
+	}
+
+	/**
+	 * Handles HTTP GET requests by retrieving orders information and forwarding
+	 * the request to the adminDashboard JSP page.
+	 * 
+	 * @param request  The HttpServletRequest object containing the request data.
+	 * @param response The HttpServletResponse object used to return the response.
+	 * @throws ServletException If an error occurs during request processing.
+	 * @throws IOException      If an input or output error occurs.
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Retrieve all student information from the AdminDashboardService
+		request.setAttribute("orderList", orderService.getRecentOrders());
+		request.setAttribute("totalProduct", dashboardService.getTotalProducts());
+		request.setAttribute("totalsales", dashboardService.getTotalSales());
+		request.setAttribute("total", dashboardService.getTotalUsers());
+
+		// Forward the request to the dashboard JSP for rendering
+		request.getRequestDispatcher(RedirectionUtil.adminDashboardUrl).forward(request, response);
+	}
+
+}
